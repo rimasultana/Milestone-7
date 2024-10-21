f881aa1a85bdc7fd71cbb64a06f0bd7cca1e1584
rimasultana <rimasultana20225@gmail.com>
2024-10-21 22:52:33 +0600

    complete

--- my-rest-countries-react/src/Components/Country/Country.jsx
+++ my-rest-countries-react/src/Components/Country/Country.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Country({ country, handleClickFlags,handleClickVisited }) {
-  const { name, area, flags, cca3,region } = country;
+export default function Country({
+  country,
+  handleClickFlags,
+  handleClickVisited,
+}) {
+  const { name, area, flags, cca3, region } = country;
+
+  const [added, setAdded] = useState(false);
+  const handleAddedClick = () => {
+    setAdded(!added);
+  };
 
   return (
-    <div className="card card-compact bg-base-100  shadow-xl">
+    <div
+      className={`${
+        added ? "bg-lime-300 " : " "
+      } card card-compact bg-base-100  shadow-xl`}
+    >
       <h2 className="font-bold py-3 text-center">Name: {name.common}</h2>
       <figure>
-        <img className="object-cover w-96 rounded-lg h-[250px]"
+        <img
+          className="object-cover w-96 rounded-lg h-[250px]"
           src={flags.png}
           alt="Shoes"
         />
@@ -17,9 +31,21 @@ export default function Country({ country, handleClickFlags,handleClickVisited }
         <p className="font-semibold">Area: {area}</p>
         <p className="font-semibold">{cca3}</p>
         <div className="card-actions justify-center">
-          <button className="btn bg-lime-500">Added</button>
-          <button onClick={()=>handleClickVisited(name.common)} className="btn bg-lime-500">Visited</button>
-          <button onClick={()=>handleClickFlags(flags.png)} className="btn bg-lime-500">Add flag</button>
+          <button onClick={handleAddedClick} className="btn bg-lime-500">
+            Added
+          </button>
+          <button
+            onClick={() => handleClickVisited(name.common)}
+            className="btn bg-lime-500"
+          >
+            Visited
+          </button>
+          <button
+            onClick={() => handleClickFlags(flags.png)}
+            className="btn bg-lime-500"
+          >
+            Add flag
+          </button>
         </div>
       </div>
     </div>
